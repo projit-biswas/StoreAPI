6d640cd5cb637daf3c15855066819b236d3e283c
Projit Biswas <projit.biswas99@gmail.com>
2025-07-01 11:54:41 +0530

    add clear cart functionality.

--- src/main/java/com/projit/storeApp/controllers/CartController.java
+++ src/main/java/com/projit/storeApp/controllers/CartController.java
@@ -116,4 +116,17 @@ public class CartController {
 		cartRepository.save(cart);
 		return ResponseEntity.noContent().build();
 	}
+
+	@DeleteMapping("/{cartId}/items")
+	public ResponseEntity<Void> clearCart(@PathVariable UUID cartId){
+		var cart = cartRepository.getCartWithItems(cartId).orElse(null);
+		if (cart == null) {
+			ResponseEntity.status(HttpStatus.NOT_FOUND).body(
+					Map.of("error", "Cart not found")
+			);
+		}
+		cart.clear();
+		cartRepository.save(cart);
+		return ResponseEntity.noContent().build();
+	}
 }
