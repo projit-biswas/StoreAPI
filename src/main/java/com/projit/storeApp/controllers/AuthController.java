17b5063f9519bdec5404363346dd4c8a9a774b0c
Projit Biswas <projit.biswas99@gmail.com>
2025-07-12 23:12:34 +0530

    generating jwt token.

--- src/main/java/com/projit/storeApp/controllers/AuthController.java
+++ src/main/java/com/projit/storeApp/controllers/AuthController.java
@@ -1,6 +1,8 @@
 package com.projit.storeApp.controllers;
 
+import com.projit.storeApp.dtos.JwtResponse;
 import com.projit.storeApp.dtos.LoginRequest;
+import com.projit.storeApp.secvices.JwtService;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -17,15 +19,17 @@ import org.springframework.web.bind.annotation.RestController;
 public class AuthController {
 
 	private AuthenticationManager authenticationManager;
+	private JwtService jwtService;
 
 	@PostMapping("/login")
-	public ResponseEntity<Void> login(@Valid @RequestBody LoginRequest request){
+	public ResponseEntity<JwtResponse> login(@Valid @RequestBody LoginRequest request){
 		authenticationManager.authenticate(
 				new UsernamePasswordAuthenticationToken(
 						request.getEmail(),
 						request.getPassword()
 				)
 		);
-		return ResponseEntity.ok().build();
+		var token = jwtService.generateToken(request.getEmail());
+		return ResponseEntity.ok(new JwtResponse(token));
 	}
 }
---
+++ src/main/java/com/projit/storeApp/dtos/JwtResponse.java
@@ -0,0 +1,10 @@
+package com.projit.storeApp.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@AllArgsConstructor
+@Data
+public class JwtResponse {
+	private String token;
+}
