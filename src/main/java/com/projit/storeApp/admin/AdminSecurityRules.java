1a1b32a99c0381ca2d332eb7d089b541f830dff0
Projit Biswas <projit.biswas99@gmail.com>
2025-08-01 22:09:24 +0530

    change admin security rules.

--- src/main/java/com/projit/storeApp/admin/AdminSecurityRules.java
+++ src/main/java/com/projit/storeApp/admin/AdminSecurityRules.java
@@ -1,7 +1,6 @@
 package com.projit.storeApp.admin;
 
 import com.projit.storeApp.common.SecurityRules;
-import com.projit.storeApp.users.Role;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AuthorizeHttpRequestsConfigurer;
 import org.springframework.stereotype.Component;
@@ -10,6 +9,6 @@ import org.springframework.stereotype.Component;
 public class AdminSecurityRules implements SecurityRules {
 	@Override
 	public void configure(AuthorizeHttpRequestsConfigurer<HttpSecurity>.AuthorizationManagerRequestMatcherRegistry registry) {
-		registry.requestMatchers("/api/admin/**").hasRole(Role.ADMIN.name());
+		registry.requestMatchers("/api/admin/**").permitAll();
 	}
 }
