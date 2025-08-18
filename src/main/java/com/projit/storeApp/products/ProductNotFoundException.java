210afb3049477c1cf38fc3f0ac94c516b94de1df
Projit Biswas <projit.biswas99@gmail.com>
2025-08-18 12:06:08 +0530

    refactor exception handling.

--- src/main/java/com/projit/storeApp/products/ProductNotFoundException.java
+++ src/main/java/com/projit/storeApp/products/ProductNotFoundException.java
@@ -4,4 +4,8 @@ public class ProductNotFoundException extends RuntimeException{
 	public ProductNotFoundException(String message) {
 		super(message);
 	}
+
+	public ProductNotFoundException() {
+		super("Product not found");
+	}
 }
